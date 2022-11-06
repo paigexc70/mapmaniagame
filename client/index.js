2bc4b4361606c6ccb1921aed59f173e60632b018
paigexc70 <catherinep1090@gmail.com>
2022-11-06 17:46:40 -0600

    small fix

--- client/index.js
+++ client/index.js
@@ -21,50 +21,53 @@ function initMap() {
     var marker = new google.maps.Marker
     ({position:{lat:48.86, lng:2.29}, map:gMap,
     icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    title: 'Eiffel Tower',});
+    title: 'Eiffel Tower',
+    });
     
 
-    var marker = new google.maps.Marker
+    var marker2 = new google.maps.Marker
     ({position:{lat:51.51, lng:-0.12}, map:gMap,
         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     });
 
     
-    var marker = new google.maps.Marker
+    var marker3 = new google.maps.Marker
     ({position:{lat:29.97, lng:31.13}, map:gMap,
         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     });
 
 
-    var marker = new google.maps.Marker
+    var marker4 = new google.maps.Marker
     ({position:{lat:40.75, lng:-73.98}, map:gMap,
         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     });
 
 
-    var marker = new google.maps.Marker
+    var marker5 = new google.maps.Marker
     ({position:{lat:-22.94, lng:-43.20}, map:gMap, 
         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     });
     
 
-    var marker = new google.maps.Marker
+    var marker6 = new google.maps.Marker
     ({position:{lat:41.89, lng:12.49}, map:gMap,
         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     });
 
 
-    var marker = new google.maps.Marker
+    var marker7 = new google.maps.Marker
     ({position:{lat:-33.85, lng:151.21}, map:gMap, 
         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     });
 
 
-    var marker = new google.maps.Marker
+    var marker8 = new google.maps.Marker
     ({position:{lat:27.98, lng:86.92}, map:gMap, 
         icon:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
     });
 
+   
+	city = document.querySelector( '.reg-input-city' );
 
     google.maps.event.addListener(gMap, 'idle', function(){
         updateGame()
@@ -121,3 +124,4 @@ function updateGame() {
 function initApplication() {
     console.log('Map mania - starting');
 }
+
